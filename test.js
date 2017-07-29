8d3ac08beb94c062ab36d6ca1bb58d3adb0613d1
Paul du Pavillon <p.dupavillon@gmail.com>
2017-07-29 12:22:34 -0500

    refactoring tests

--- test.js
+++ test.js
@@ -37,6 +37,29 @@ class HttpTestHelper{
   };
 }
 
+class RecaptchaWrapper{
+  constructor(isMiddleware, opt){
+    this.isMiddleware = isMiddleware;
+    recaptcha.init('SITE_KEY', 'SECRET_KEY', opt);
+  }
+  static Init(isMiddleware, opt){
+    return new RecaptchaWrapper(isMiddleware, opt);
+  }
+  render(req){
+    if (this.isMiddleware) { var req = {}; recaptcha.middleware.render(req,{},function(){}); return req.recaptcha};
+    return recaptcha.render();
+  }
+  verify(req, cb){
+    if (this.isMiddleware) {
+      recaptcha.middleware.verify(req,{},function(){
+        req.recaptcha.should.be.ok();
+        cb(req.recaptcha.error);
+      }); 
+    }
+    else recaptcha.verify(req,cb);
+  }
+}
+
 describe('Recaptcha', function() {
 
   beforeEach(function() {
@@ -88,119 +111,86 @@ describe('Recaptcha', function() {
     (function(){ recaptcha.init('SITE_KEY'); }).should.throw('secret_key is required');
   });
 
-  describe('Direct use', function() {
-
-    it('Get Html', function(){
-      recaptcha.init('SITE_KEY','SECRET_KEY');
-      var result = recaptcha.render();
+  function Render(){
+      var result = RecaptchaWrapper.Init(this.isMiddleware).render();
       var expected = '<script src="//'+api_url+'" async defer></script>'+
       '<div class="g-recaptcha" data-sitekey="SITE_KEY"></div>';
       result.should.be.equal(expected);
-    });
+  }
+  function RenderWithOption(){
+    var result = RecaptchaWrapper.Init(this.isMiddleware ,{
+      onload:'cb',
+      render:'explicit',
+      hl:'fr',
+      theme:'dark',
+      type:'audio',
+      callback:'callback',
+      expired_callback:'expired_callback',
+      size:'size',
+      tabindex:'tabindex'
+    }).render();
+    var expected = '<script src="//'+api_url+'?onload=cb&render=explicit&hl=fr" async defer></script>'+
+    '<div class="g-recaptcha" data-sitekey="SITE_KEY" data-theme="dark" data-type="audio" data-callback="callback" data-expired-callback="expired_callback" data-size="size" data-tabindex="tabindex"></div>';
+    result.should.be.equal(expected);
+  }
 
-    it('Get Html with options', function(){
-      recaptcha.init('SITE_KEY','SECRET_KEY',{
-        onload:'cb',
-        render:'explicit',
-        hl:'fr',
-        theme:'dark',
-        type:'audio',
-        callback:'callback',
-        expired_callback:'expired_callback',
-        size:'size',
-        tabindex:'tabindex'
-      });
-      var result = recaptcha.render();
-      var expected = '<script src="//'+api_url+'?onload=cb&render=explicit&hl=fr" async defer></script>'+
-      '<div class="g-recaptcha" data-sitekey="SITE_KEY" data-theme="dark" data-type="audio" data-callback="callback" data-expired-callback="expired_callback" data-size="size" data-tabindex="tabindex"></div>';
-      result.should.be.equal(expected);
-    });
-
-
-    it('Verify', function(done){
+  function Verify(done){
       this.httpTestHelper.build();
-      recaptcha.init('SITE_KEY','SECRET_KEY');
-
-      recaptcha.verify({body:{'g-recaptcha-response':'1234578910'}}, function(error){
+      RecaptchaWrapper.Init(this.isMiddleware).verify({body:{'g-recaptcha-response':'1234578910'}}, function(error){
         (error === null).should.be.true();
         this.httpTestHelper.checkValidationQueryString('secret=SECRET_KEY&response=1234578910');
-        done();        
-      }.bind(this));
-    });
-
-    it('Verify client ip - x-forwarded-for header', function(done){
-      this.httpTestHelper.build();
-      recaptcha.init('SITE_KEY','SECRET_KEY', {checkremoteip: true});
-
-      recaptcha.verify({body:{'g-recaptcha-response':'1234578910'},headers:{'x-forwarded-for':'10.0.0.1'}}, function(error){
-        (error === null).should.be.true();
-        this.httpTestHelper.checkValidationQueryString('secret=SECRET_KEY&response=1234578910&remoteip=10.0.0.1');
         done();
       }.bind(this));
-    });
-
-    it('Verify client ip - connection remote addr', function(done){
-      this.httpTestHelper.build();
-      recaptcha.init('SITE_KEY','SECRET_KEY', {checkremoteip: true});
-
-      recaptcha.verify({body:{'g-recaptcha-response':'1234578910'}, connection:{remoteAddress:'10.0.0.1'}}, function(error){
-        (error === null).should.be.true();
-        this.httpTestHelper.checkValidationQueryString('secret=SECRET_KEY&response=1234578910&remoteip=10.0.0.1');
-        done();
-      }.bind(this));
-    });
-
-    it('Verify with error', function(done){
+  }
+  function VerifyError(done){
       this.httpTestHelper.withErrorCode('invalid-input-response').build();
 
-      recaptcha.init('SITE_KEY','SECRET_KEY');
-
-      recaptcha.verify({body:{'g-recaptcha-response':'1234578910'}},function(error){
+      RecaptchaWrapper.Init(this.isMiddleware).verify({body:{'g-recaptcha-response':'1234578910'}},function(error){
         error.should.be.equal("invalid-input-response");
         this.httpTestHelper.checkValidationQueryString('secret=SECRET_KEY&response=1234578910');
         done();
+      }.bind(this));    
+  }
+  function VerifyClientIpHeader(done){
+      this.httpTestHelper.build();
+
+      RecaptchaWrapper.Init(this.isMiddleware, {checkremoteip: true}).verify({body:{'g-recaptcha-response':'1234578910'},headers:{'x-forwarded-for':'10.0.0.1'}}, function(error){
+        (error === null).should.be.true();
+        this.httpTestHelper.checkValidationQueryString('secret=SECRET_KEY&response=1234578910&remoteip=10.0.0.1');
+        done();
+      }.bind(this));    
+  }
+  function VerifyClientIpRemoteAddr(done){
+      this.httpTestHelper.build();
+      
+      RecaptchaWrapper.Init(this.isMiddleware, {checkremoteip: true}).verify({body:{'g-recaptcha-response':'1234578910'}, connection:{remoteAddress:'10.0.0.1'}}, function(error){
+        (error === null).should.be.true();
+        this.httpTestHelper.checkValidationQueryString('secret=SECRET_KEY&response=1234578910&remoteip=10.0.0.1');
+        done();
       }.bind(this));
+  }
+
+  describe('Direct use', function() {
+    beforeEach(function() {
+      this.isMiddleware = false; 
     });
+    it('Render', Render.bind(this));
+    it('Render with options', RenderWithOption.bind(this));
+    it('Verify', function(done){ Verify.call(this, done); });
+    it('Verify with error', function(done){ VerifyError.call(this, done); });
+    it('Verify client ip - x-forwarded-for header', function(done){ VerifyClientIpHeader.call(this, done); });
+    it('Verify client ip - connection remote addr', function(done){ VerifyClientIpRemoteAddr.call(this, done); });
   });
 
   describe('Middleware use', function() {
-    it('Render Html', function(){
-      var req = {};
-      var expected = '<script src="//'+api_url+'" async defer></script>'+
-      '<div class="g-recaptcha" data-sitekey="SITE_KEY"></div>';
-
-      recaptcha.init('SITE_KEY','SECRET_KEY');
-      recaptcha.middleware.render(req,{},function(){});
-
-      req.recaptcha.should.be.equal(expected);
-    });
-
-    it('Verify', function(done){
-      var req = {body:{'g-recaptcha-response':'1234578910'}};
-      this.httpTestHelper.build();
-
-      recaptcha.init('SITE_KEY','SECRET_KEY');
-
-      recaptcha.middleware.verify(req,{}, function(){
-        req.recaptcha.should.be.ok;
-        (req.recaptcha.error === null).should.be.true();
-        this.httpTestHelper.checkValidationQueryString('secret=SECRET_KEY&response=1234578910');
-        done();
-      }.bind(this));
-    });
-
-    it('Verify with error', function(done){
-      this.httpTestHelper.withErrorCode('invalid-input-response').build();
-      var req = {body:{'g-recaptcha-response':'1234578910'}};
-
-      recaptcha.init('SITE_KEY','SECRET_KEY');
-
-      recaptcha.middleware.verify(req,{}, function(){
-        req.recaptcha.should.be.ok;
-        req.recaptcha.error.should.be.equal('invalid-input-response');
-        this.httpTestHelper.checkValidationQueryString('secret=SECRET_KEY&response=1234578910');
-        done();
-      }.bind(this));
-    });
+    beforeEach(function() {
+      this.isMiddleware = true; 
+    });    
+    it('Render', Render.bind(this));
+    it('Render with options', RenderWithOption.bind(this));
+    it('Verify', function(done){ Verify.call(this, done); });
+    it('Verify with error', function(done){ VerifyError.call(this, done); });
+    it('Verify client ip - x-forwarded-for header', function(done){ VerifyClientIpHeader.call(this, done); });
+    it('Verify client ip - connection remote addr', function(done){ VerifyClientIpRemoteAddr.call(this, done); });
   });
 });
